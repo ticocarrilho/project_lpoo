13857e90707d14439fa055b66c16d01b87703a7f
Paulo Ricardo <pr838908@gmail.com>
2019-09-19 10:30:39 -0300

    alimentos class

--- src/obj/AbstractAlimentos.java
+++ src/obj/AbstractAlimentos.java
@@ -1,5 +1,21 @@
 package obj;
 
-public class AbstractAlimentos {
+public class AbstractAlimentos extends AbstractProdutos{
+	private boolean produtoLiquido;
+
+	public AbstractAlimentos(int idProduto, String nomeProduto, double precoProduto, double pesoProduto,
+			String medidaPeso, Fornecedores fornecedor, boolean produtoLiquido) {
+		super(idProduto, nomeProduto, precoProduto, pesoProduto, medidaPeso, fornecedor);
+		this.produtoLiquido = produtoLiquido;
+	}
+
+	public boolean isProdutoLiquido() {
+		return produtoLiquido;
+	}
+
+	public void setProdutoLiquido(boolean produtoLiquido) {
+		this.produtoLiquido = produtoLiquido;
+	}
+	
 
 }
